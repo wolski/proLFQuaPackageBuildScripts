1a49793e9801eef4e3a841a19136ba8e6d42c1b2
Witold Wolski <wewolski@gmail.com>
2022-01-26 11:00:17 +0100

    update path variables for MAC

--- buildProlfqua.bat
+++ buildProlfqua.bat
@@ -1,32 +1,32 @@
-set /A Install = 1
+Install=1
+
+if [ Install = 1 ]; then
+    mkdir $HOME/__checkout/proLFQuaPackageBuildScripts/r-site-library_prolfqua
+fi
+
+R_LIBS_USER=
+export R_LIBS_SITE="$HOME/__checkout/proLFQuaPackageBuildScripts/r-site-library_prolfqua"
+
+mkdir $HOME/__checkout/proLFQuaPackageBuildScripts/test_build_prolfqua
+
 
 if %Install% == 1 (
-    mkdir "d:\prolfquaPackageBuilds\r-site-library_prolfqua"
+    rm -Rf $HOME/__checkout/proLFQuaPackageBuildScripts/r-site-library_prolfqua/*
+    rm -Rf $HOME/__checkout/proLFQuaPackageBuildScripts/test_build_prolfqua/*
+    
+    R --vanilla -e "install.packages('remotes', repos = 'https://cloud.r-project.org' )"
+    R --vanilla -e "remotes::install_gitlab('wolski/prolfquadata', host = 'gitlab.bfabric.org')"
+    R --vanilla -e "BiocManager::install('vsn')"
+    R --vanilla -e "remotes::install_github('wolski/prozor')"
+    R --vanilla -e "install.packages('pander', repos = 'https://cloud.r-project.org' )"
+    
+    Rscript InstallDependencies.R wolski prolfqua reinst > InstallDependencies_prolfqua.log 2>&1
 )
 
-set R_LIBS_USER=
-set "R_LIBS_SITE=d:\prolfquaPackageBuilds\r-site-library_prolfqua"
+rm -Rf $HOME/__checkout/proLFQuaPackageBuildScripts/test_build_prolfqua/*
+Rscript runBuild.R wolski prolfqua > runBuild_prolfqua.log 2>&1
 
-mkdir "d:\prolfquaPackageBuilds\test_build_prolfqua\"
-
-
-if %Install% == 1 (
-    rm -Rf d:/prolfquaPackageBuilds/r-site-library_prolfqua/*
-    rm -Rf d:/prolfquaPackageBuilds/test_build_prolfqua/*
-    
-    R -e "install.packages('remotes', repos = 'https://cloud.r-project.org' )"
-    
-    R -e "remotes::install_gitlab('wolski/prolfquadata', host = 'gitlab.bfabric.org')"
-    R -e "BiocManager::install('vsn')"
-    R -e "remotes::install_github('wolski/prozor')"
-  
-    Rscript.exe InstallDependencies.R wolski prolfqua reinst > InstallDependencies_prolfqua.log 2>&1
-)
-
-rm -Rf d:/prolfquaPackageBuilds/test_build_prolfqua/*
-Rscript.exe runBuild.R wolski prolfqua > runBuild_prolfqua.log 2>&1
-
-rm -Rf d:/prolfquaPackageBuilds/test_build_prolfquabenchmark/*
+rm -Rf $HOME/__checkout/proLFQuaPackageBuildScripts/test_build_prolfquabenchmark/*
 Rscript.exe runBuild.R wolski prolfquabenchmark  > runBuild_prolfquabenchmark.log 2>&1
 
 
